8b2644230f8dc593b5a8d473a3c0b0f4af164ed0
nickarshadi <1477@holbertonschool.com>
2021-01-27 20:52:58 +0000

    Update app.py file.

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,22 @@
 """Module for Flask REST application."""
 from flask import Flask
 from models import storage
-from api.vi.views import app_views
+from api.v1.views import app_views
 from os import getenv
 from flask import make_response
-from flask_cors import CORS
+
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def closedb(foo):
+    """Close db session."""
+    storage.close()
+
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
+    app.run(host, port, threaded=True)
