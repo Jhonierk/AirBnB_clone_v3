3b8071d133f16e0a978b338e977519b88440b326
nickarshadi <1477@holbertonschool.com>
2021-01-27 21:25:18 +0000

    Add Error 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,6 +16,12 @@ def closedb(foo):
     storage.close()
 
 
+@app.errorhandler(404)
+def not_found(error):
+    """Error handle function."""
+    return (jsonify({"error": "Not found"}), 404)
+
+
 if __name__ == "__main__":
     host = getenv("HBNB_API_HOST", "0.0.0.0")
     port = getenv("HBNB_API_PORT", 5000)
