2b5aec0e58ad2fbe79cc0f9b0e3641aaaef42a9f
nickarshadi <1477@holbertonschool.com>
2021-01-28 04:54:41 +0000

    Make file shorter.

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -23,12 +23,20 @@ def get_state(state_id=None):
     return jsonify(state.to_dict())
 
 
-@app_views.route("/states/<state_id>", strict_slashes=False, methods=["DELETE"])
-def delete_state(state_id):
-    """Delete a State object."""
-    if state_id is None:
+@app_views.route('/states/<string:state_id>', methods=['GET'],
+                 strict_slashes=False)
+def get_state(state_id):
+    """get state information for specified state"""
+    state = storage.get("State", state_id)
+    if state is None:
         abort(404)
+    return jsonify(state.to_dict())
 
+
+@app_views.route('/states/<string:state_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_state(state_id):
+    """deletes a state based on its state_id"""
     state = storage.get("State", state_id)
     if state is None:
         abort(404)
@@ -37,9 +45,9 @@ def delete_state(state_id):
     return (jsonify({}))
 
 
-@app_views.route("/states", methods=["POST"], strict_slashes=False)
+@app_views.route('/states/', methods=['POST'], strict_slashes=False)
 def post_state():
-    """Create a new State"""
+    """create a new state"""
     if not request.get_json():
         return make_response(jsonify({'error': 'Not a JSON'}), 400)
     if 'name' not in request.get_json():
@@ -49,10 +57,10 @@ def post_state():
     return make_response(jsonify(state.to_dict()), 201)
 
 
-@app_views.route("/states/<string:state_id>", methods=["PUT"],
+@app_views.route('/states/<string:state_id>', methods=['PUT'],
                  strict_slashes=False)
 def put_state(state_id):
-    """Update a state."""
+    """update a state"""
     state = storage.get("State", state_id)
     if state is None:
         abort(404)
